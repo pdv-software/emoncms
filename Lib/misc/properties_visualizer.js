7b8b341555009ef615d00561d5de53f92df31204
Christian Sack <cs@pdv-software.de>
2017-03-15 09:41:27 +0100

    Fixing bug that prevented invocation of range validator listeners.

--- Lib/misc/properties_visualizer.js
+++ Lib/misc/properties_visualizer.js
@@ -2,44 +2,45 @@ function PropertiesVisualizer(){
   this.numberValidatorListeners = [];
   this.rangeValidatorListeners = [];
 }
-
+PropertiesVisualizer.prototype.BELOW_RANGE_MIN = 0;
+PropertiesVisualizer.prototype.ABOVE_RANGE_MAX = 1;
 PropertiesVisualizer.prototype.visualize = function (properties, container, initialValues){
-				   var inputs = [];
+		   var inputs = [];
                    for(var index in properties){
 	                	
-						var prop = properties[index];						
+				var prop = properties[index];						
 	                	var label = $('<label></label>');
-						label.attr('for', prop.name);
-						label.html(prop.name);
+				label.attr('for', prop.name);
+				label.html(prop.name);
 	                	var input;
 	                	switch(prop.inputType){
 	                		case 'free':
 	                		  input = $('<input type="text"></input>');
-							  if(initialValues.hasOwnProperty(prop.name)){
-								input.val(initialValues[prop.name])  
-							  }else{
-	                		    if(prop.defaultValue){
-	                			  input.val(prop.defaultValue);
+					  if(initialValues.hasOwnProperty(prop.name)){
+					    input.val(initialValues[prop.name])  
+					  }else{
+	                		    if(typeof prop.defaultValue !== "undefined"){
+	                			input.val(prop.defaultValue);
 	                		    }
-							  }
-					    if(prop.dataType == 'number'){
+				          }
+					  if(prop.dataType == 'number'){
 	                		    input.on("focusout", function(e){
-								  var val = $(this).val();
-	                			  if(!$.isNumeric(val)){
-	                				  for(var l in this.numberValidatorListeners){
-										  l(this, val);
-							  }
-	                			  }  
+						var val = $(this).val();
+	                			if(!$.isNumeric(val)){
+                                                    for(var l in this.numberValidatorListeners){
+							l(this, val);
+                                                    }
+	                			}  
 	                		    });
-					    }
+					  }
 	                		  break;
 	                		case 'range':
 	                		  input = $('<input type="text"></input>');
 	                		  if(initialValues.hasOwnProperty(prop.name)){
-								input.val(initialValues[prop.name])  
+					    input.val(initialValues[prop.name])  
 					  }else{
-	                		    if(prop.defaultValue){
-	                			  input.val(prop.defaultValue);
+	                		    if(typeof prop.defaultValue !== "undefined"){
+	                		      input.val(prop.defaultValue);
 	                		    }
 					  }
 					  if( (typeof prop.rangeMin === "number") && (typeof prop.rangeMax === "number") ){
@@ -49,52 +50,53 @@ PropertiesVisualizer.prototype.visualize = function (properties, container, init
 					  }else if(typeof prop.rangeMax === "number"){
 					    label.html(prop.name + " ( max. " + prop.rangeMax + ")");   
 					  }
+                                          var pv = this;
 	                		  input.on("focusout", function(e){
 	                			var val = $(this).val();
 	                			if(!$.isNumeric(val)){
-	                				for(var l in this.numberValidatorListeners){
-										l(this, val);
-									}
+                                                    for(var l in this.numberValidatorListeners){
+                                                        l(this, val);
+                                                    }
 	                			} else{
-									var floatVal = parseFloat(val);
-	                				if(prop.rangeMin && floatVal < prop.rangeMin){
-	                				  for(var l in this.rangeValidatorListeners){
-										l(this, floatVal, true, prop.rangeMin);
-									  }	
-	                				} else if(prop.rangeMax && floatVal > prop.rangeMax){
-	                				  for(var l in this.rangeValidatorListeners){
-										l(this, floatVal, false, prop.rangeMax);
-									  }		
-	                				}
+                                                    var floatVal = parseFloat(val);
+                                                    if((typeof prop.rangeMin === "number") && (floatVal < prop.rangeMin)){
+                                                        for(var l in pv.rangeValidatorListeners){
+                                                            l(this, floatVal, pv.BELOW_RANGE_MIN, prop.rangeMin);
+                                                        }	
+                                                    } else if((typeof prop.rangeMax === "number") && (floatVal > prop.rangeMax)){
+	                				for(var l in pv.rangeValidatorListeners){
+                                                            l(this, floatVal, pv.BELOW_RANGE_MIN, prop.rangeMax);
+                                                        }		
+                                                    }
 	                			} 
 	                		  });
 	                		  break;
 	                		case 'selection':
-							  input = $('<select></select>');
-							  var val = null;
-							  if(initialValues.hasOwnProperty(prop.name)){
-								val = initialValues[prop.name]; 
-							  }else{
-	                		    if(prop.defaultValue){
-	                			  val = prop.defaultValue;
-	                		    }
-							  }
-							  $.each(prop.values, function(key, value){
-								  var option = $("<option></option>").attr("value", value.value).text(value.display);
-								  if(val === value.value){
-									  option.attr("selected", "selected");
-								  }
-								  input.append(option);
-							  });
+                                            input = $('<select></select>');
+                                            var val = null;
+                                            if(initialValues.hasOwnProperty(prop.name)){
+						val = initialValues[prop.name]; 
+                                            }else{
+                                                if(typeof prop.defaultValue !== "undefined"){
+                                                    val = prop.defaultValue;
+                                                }
+                                            }
+                                            $.each(prop.values, function(key, value){
+                                                var option = $("<option></option>").attr("value", value.value).text(value.display);
+                                                if(val === value.value){
+                                                    option.attr("selected", "selected");
+                                                }
+						input.append(option);
+                                            });
 	                		  break;
 	                	}
-						input.attr('id', prop.name);
-						if(prop.description){
-							input.attr('title', prop.description);
-						}
-						label.appendTo(container);
+				input.attr('id', prop.name);
+				if(prop.description){
+                                    input.attr('title', prop.description);
+				}
+				label.appendTo(container);
 	                	input.appendTo(container);
-						inputs.push(input);
+				inputs.push(input);
 	                	$("<br><br>").appendTo(container);
 	                }
 					return inputs;
